f5ce2f526d21b7154dbb27b9f67d56bc2d8c9b95
Arielle <arielle@MacBook-Pro-92.local>
2023-02-20 14:27:06 -0500

    Arielle changes 2/20

--- main.js
+++ main.js
@@ -9,32 +9,166 @@ const FRAME1 = d3.select("#vis1")
                     .attr("width", FRAME_WIDTH)
                     .attr("class", "frame"); 
 
-//defining the scales
-const xScale = d3.scaleLinear()
-.range([MARGINS.left, FRAME_WIDTH - MARGINS.right]);
 
-const yScale = d3.scaleLinear()
-.range([FRAME_HEIGHT - MARGINS.bottom, MARGINS.top]);
-   
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
 
-d3.csv("data/scatter-data.csv").then((data) => { 
-    xScale.domain([0, d3.max(data, d => d.x)]);
-    yScale.domain([0, d3.max(data, d => d.y)]);
 
-    // let's check our data
-    console.log(data); //Notice this data has 2 columns
-                        // to access data in a column, use .
-  
+// This time, let's define a function that builds our plot
+function build_interactive_plot() {
+
+
+  d3.csv("data/scatter-data.csv").then((data) => { 
+    // make the domains go from 0 to 10 for the ticks
+
+    // Build plot inside of .then 
+    // find max X
+    const MAX_X1 = d3.max(data, (d) => { return parseInt(d.x); });
+
+    // Define scale functions that maps our data values 
+    // (domain) to pixel values (range)
+    const X_SCALE1 = d3.scaleLinear() 
+                      .domain([0, 10]) // add some padding  
+                      .range([0, VIS_WIDTH]); 
+
+    const Y_SCALE1 = d3.scaleLinear() 
+    .domain([0, 10]) // add some padding  
+    .range([VIS_HEIGHT, 0]); 
+
     // add our dots with styling 
     FRAME1.selectAll("circle") 
         .data(data) // this is passed from  .then()
         .enter()  
         .append("circle")
-            
-          .attr("cx", (d) => { return xScale(d.x); }) // use x for cx
-          .attr("cy", (d) => { return yScale(d.y); }) // use y for cy
+          .attr("class", "point") // add class
+          .attr("cx", (d) => { return X_SCALE1(d.x) + MARGINS.left }) // use x for cx
+          .attr("cy", (d) =>{ return Y_SCALE1(d.y) + MARGINS.bottom; }) // use y for cy
           .attr("r", 7)  // set r 
           .attr("fill", "#0000FF"); // fill 
+        
+
+       // Add an x axis to the vis  
+       FRAME1.append("g") 
+       .attr("transform", "translate(" + MARGINS.left + 
+             "," + (VIS_HEIGHT + MARGINS.top) + ")") 
+       .call(d3.axisBottom(X_SCALE1).ticks(10)) 
+         .attr("font-size", '20px'); 
+
+      // add a y axis to the vis
+      FRAME1.append("g") 
+      .attr("transform", "translate(" + MARGINS.left + "," + MARGINS.top + ")") 
+      .call(d3.axisLeft(Y_SCALE1).ticks(10)) 
+        .attr("font-size", '20px'); 
+
+
+    // getting the coordinates of a circle when you click on it
+    const rightColumn = document.querySelector(".right-column");
+
+    // Function that handles click event on the points in the left column and the right column
+    function handlePointClick(event) {
+      // Get the point that was clicked
+      const point = event.target;
+
+      // toggle selected class for the point that was clicked
+      point.classList.toggle("selected");
+
+      // add/remove border class for the point that was
+      if (point.classList.contains("selected")) {
+        point.classList.add("border");
+      } else {
+        point.classList.remove("border");
+      }
+      
+      // remove any previously added coordinates in the right column
+      const oldCoordinates = rightColumn.querySelectorAll('.coordinate');
+      oldCoordinates.forEach(old => old.remove());
+
+      // get the selected coordinates
+      const x = point.getAttribute("cx");
+      const y = point.getAttribute("cy");
+
+      // create a new paragraph element with the selected coordinates
+      const newCoordinates = document.createElement('p');
+      newCoordinates.classList.add('coordinate');
+     
+
+  newCoordinates.innerHTML = `Selected Coordinate: (${x}, ${y})`;
+
+  // add the new paragraph to the beginning of the right column
+  rightColumn.insertBefore(newCoordinates, rightColumn.firstChild);
+  }
+
+  // get the points in the left column
+  const points = document.querySelectorAll(".point");
+
+  // adds event listener for clicking each point
+  points.forEach(point => point.addEventListener("click", handlePointClick));
+
+
+  // add event listener to the form in html code
+  const form = document.querySelector("form");
+
+  form.addEventListener("submit", function(e) {
+  e.preventDefault(); // prevent default action
+
+  // Get the select element inside the form
+  var select = this.querySelectorAll("select");
+
+  // Loop through the select elements and get the selected value
+  for (var i = 0; i < select.length; i++) {
+    if (select[i].value !== "0") {
+      // remove any previously added coordinates in the right column
+      const oldCoordinates = rightColumn.querySelectorAll('.coordinate');
+      oldCoordinates.forEach(old => old.remove());
+
+      // create a new paragraph element with the selected coordinate
+      const newCoordinates = document.createElement('p');
+      newCoordinates.classList.add('coordinate');
+      newCoordinates.innerHTML = `Selected Coordinate ${i+1}: ${select[i].value}`;
+
+      // add the new paragraph to the beginning of the right column
+      rightColumn.insertBefore(newCoordinates, rightColumn.firstChild);
+    }
+  }
+  });
+;
+
+function addCircle() {
+  // Get the x and y values entered by the user
+  /// Get the x and y values entered by the user
+  const xValue = document.querySelector("#x").value;
+  const yValue = document.querySelector("#y").value;
   
-  }); // .then is closed here 
+  // Create a new circle element and add it to the plot
+  FRAME1.append("circle")
+    .attr("class", "point") // add class
+    .attr("cx", X_SCALE1("xValue") + MARGINS.left) // use x for cx
+    .attr("cy", Y_SCALE1("yValue") + MARGINS.bottom) // use y for cy
+    .attr("r", 7) // set r
+    .attr("fill", "#0000FF") // fill 
+   
+  // add event listener to the new circle
+  const newCircle = FRAME1.select(":last-child");
+  newCircle.on("click", handlePointClick); 
+
+  // Add event listener to the "Add Circle" button
+const addCircleButton = document.querySelector("#add-circle");
+}
+
+
+
+  // Add event listeners
+  FRAME1.selectAll(".point")
+  .on("selected", handlePointClick)
+  .on("click", addCircle());  
+  
+
+   
+  });
+}
+
+
+
+  // call function
+build_interactive_plot();
 
